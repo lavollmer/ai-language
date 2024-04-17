60e4cef1f770c014ea713c2d916c653723aa3547
Laura <laura.e.vollmer@gmail.com>
2024-04-17 08:45:09 -0500

    adding language service

--- 06b-translator-sdk/C-Sharp/translate-text/Program.cs
+++ 06b-translator-sdk/C-Sharp/translate-text/Program.cs
@@ -4,8 +4,8 @@ using System.Text;
 using System.Collections.Generic;
 using Microsoft.Extensions.Configuration;
 using System.Threading.Tasks;
-
-// import namespaces
+using Azure;
+using Azure.AI.Translation.Text;
 
 
 
@@ -29,17 +29,42 @@ namespace translate_text
 
 
                 // Create client using endpoint and key
-
+                AzureKeyCredential credential = new(translatorKey);
+                TextTranslationClient client = new(credential, translatorRegion);
 
 
                 // Choose target language
-
+                languagesResponse = client.get_languages(scope = "translation")
+print("{} languages supported.".format(len(languagesResponse.translation)))
+print("(See https://learn.microsoft.com/azure/ai-services/translator/language-support#translation)")
+print("Enter a target language code for translation (for example, 'en'):")
+targetLanguage = "xx"
+supportedLanguage = False
+while supportedLanguage == False:
+    targetLanguage = input()
+    if  targetLanguage in languagesResponse.translation.keys():
+        supportedLanguage = True
+    else:
+        print("{} is not a supported language.".format(targetLanguage))
 
 
                 // Translate text
+string inputText = "";
+                while (inputText.ToLower() != "quit")
+                {
+                    Console.WriteLine("Enter text to translate ('quit' to exit)");
+                    inputText = Console.ReadLine();
+                    if (inputText.ToLower() != "quit")
+                    {
+                        Response<IReadOnlyList<TranslatedTextItem>> translationResponse = await client.TranslateAsync(targetLanguage, inputText).ConfigureAwait(false);
+                        IReadOnlyList<TranslatedTextItem> translations = translationResponse.Value;
+                        TranslatedTextItem translation = translations[0];
+                        string sourceLanguage = translation?.DetectedLanguage?.Language;
+                        Console.WriteLine($"'{inputText}' translated from {sourceLanguage} to {translation?.Translations[0].To} as '{translation?.Translations?[0]?.Text}'.");
+                    }
+                }
 
 
-                
 
             }
             catch (Exception ex)
--- 06b-translator-sdk/C-Sharp/translate-text/obj/Debug/net7.0/translate_text.AssemblyInfo.cs
+++ 06b-translator-sdk/C-Sharp/translate-text/obj/Debug/net7.0/translate_text.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("translate_text")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5307dfe1729ef0e16b8614d06272b4693410afaa")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+baae76af9471248dbc2152f847df14c57cc9ce22")]
 [assembly: System.Reflection.AssemblyProductAttribute("translate_text")]
 [assembly: System.Reflection.AssemblyTitleAttribute("translate_text")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
